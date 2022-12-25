56e67f80b53219b3a43e2f7a0db2d4d3b9f98130
Amit Singh <amitsingh@Amits-MacBook-Pro.local>
2022-12-25 21:04:36 +0530

    Completed welcome screen design development

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,66 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ['./*.html'],
+  mode: "jit",
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
-        greenbackground: '#A8CAD1',
-        bluebackground: '#A3BBD9',
-        halo: '#FBFBFB',
-        primary: '#A3BBD9',
-        secondary: '#D7D7D7',
-        accent: '#F8F1DB',
-        border: '#C4CBCD',
-        textcolor: '#F8F1DB'
-
+        greenbackground: "#A8CAD1",
+        bluebackground: "#A3BBD9",
+        halo: "#FBFBFB",
+        primary: "#A3BBD9",
+        secondary: "#D7D7D7",
+        accent: "#F8F1DB",
+        border: "#C4CBCD",
+        textcolor: "#F8F1DB",
+        whiteGrey: "#DAD9D9",
+        white: "#FFFFFF",
+        darkGrey: "#797979",
+        darkSecondaryGrey: "#8D8D8D",
       },
       fontFamily: {
-        body: ['EB Garamond', 'sans-serif']
+        body: ["Lato", "sans-serif"],
       },
       backgroundImage: {
-        'hero-pattern': "url('assets/imgs/bg-still.svg')",
+        "hero-pattern": "url('assets/imgs/bg-still.svg')",
       },
       animation: {
-        'blob': 'blob 7s infinite',
-        'calm': 'calm 10s linear',
-        'welcome': 'welcome 13s linear',
-        'video': 'video 15s linear',
-        'tilt': 'tilt 10s infinite linear',
+        blob: "blob 7s infinite",
+        calm: "calm 10s linear",
+        welcome: "welcome 13s linear",
+        video: "video 15s linear",
+        tilt: "tilt 10s infinite linear",
       },
       keyframes: {
         calm: {
-          '0%': { opacity: '0', transform: 'translateY(50px)' },
-          '10%': { opacity: '0.1', transform: 'translateY(40px)' },
-          '20%': { opacity: '0.5', transform: 'translateY(30px)' },
-          '30%': { opacity: '0.3', transform: 'translateY(20px)' },
-          '60%': { opacity: '0.8', transform: 'translateY(10px)' },
-          '75%': { opacity: '1', transform: 'translateY(5px)' },
-          '80%': { opacity: '1', transform: 'translateY(1px)' },
-          '95%': { opacity: '0.8', transform: 'translateY(0px)' },
-          '100%': { opacity: '0.3', transform: 'translateY(0px)' },
+          "0%": { opacity: "0", transform: "translateY(50px)" },
+          "10%": { opacity: "0.1", transform: "translateY(40px)" },
+          "20%": { opacity: "0.5", transform: "translateY(30px)" },
+          "30%": { opacity: "0.3", transform: "translateY(20px)" },
+          "60%": { opacity: "0.8", transform: "translateY(10px)" },
+          "75%": { opacity: "1", transform: "translateY(5px)" },
+          "80%": { opacity: "1", transform: "translateY(1px)" },
+          "95%": { opacity: "0.8", transform: "translateY(0px)" },
+          "100%": { opacity: "0.3", transform: "translateY(0px)" },
         },
         welcome: {
-          '0%': { opacity: '0', transform: 'translateY(50px)' },
-          '10%': { opacity: '0', transform: 'translateY(40px)' },
-          '20%': { opacity: '0', transform: 'translateY(30px)' },
-          '30%': { opacity: '0.1', transform: 'translateY(20px)' },
-          '60%': { opacity: '0.2', transform: 'translateY(10px)' },
-          '75%': { opacity: '0.8', transform: 'translateY(5px)' },
-          '80%': { opacity: '1', transform: 'translateY(1px)' },
-          '85%': { opacity: '0.9', transform: 'translateY(1px)' },
-          '95%': { opacity: '0.4', transform: 'translateY(1px)' },
-          '100%': { opacity: '0.2', transform: 'translateY(0px)' },
+          "0%": { opacity: "0", transform: "translateY(50px)" },
+          "10%": { opacity: "0", transform: "translateY(40px)" },
+          "20%": { opacity: "0", transform: "translateY(30px)" },
+          "30%": { opacity: "0.1", transform: "translateY(20px)" },
+          "60%": { opacity: "0.2", transform: "translateY(10px)" },
+          "75%": { opacity: "0.8", transform: "translateY(5px)" },
+          "80%": { opacity: "1", transform: "translateY(1px)" },
+          "85%": { opacity: "0.9", transform: "translateY(1px)" },
+          "95%": { opacity: "0.4", transform: "translateY(1px)" },
+          "100%": { opacity: "0.2", transform: "translateY(0px)" },
         },
         video: {
-          '0%': { opacity: '0', transform: 'translateY(50px)' },
-          '10%': { opacity: '0', transform: 'translateY(40px)' },
-          '20%': { opacity: '0', transform: 'translateY(30px)' },
-          '30%': { opacity: '0', transform: 'translateY(20px)' },
-          '60%': { opacity: '0', transform: 'translateY(10px)' },
-          '75%': { opacity: '0', transform: 'translateY(5px)' },
-          '80%': { opacity: '0.2', transform: 'translateY(1px)' },
-          '85%': { opacity: '0.5', transform: 'translateY(1px)' },
-          '95%': { opacity: '0.8', transform: 'translateY(1px)' },
-          '100%': { opacity: '1', transform: 'translateY(0px)' },
+          "0%": { opacity: "0" },
+          "80%": { opacity: "0.2" },
+          "85%": { opacity: "0.5" },
+          "95%": { opacity: "0.8" },
+          "100%": { opacity: "1" },
         },
         blob: {
           "0%": {
@@ -79,18 +77,18 @@ module.exports = {
           },
         },
         tilt: {
-          '0%, 50%, 100%': {
-            transform: 'rotate(0deg)',
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
           },
-          '25%': {
-            transform: 'rotate(0.5deg)',
+          "25%": {
+            transform: "rotate(0.5deg)",
           },
-          '75%': {
-            transform: 'rotate(-0.5deg)',
+          "75%": {
+            transform: "rotate(-0.5deg)",
           },
         },
-      }
+      },
     },
   },
   plugins: [],
-}
+};
